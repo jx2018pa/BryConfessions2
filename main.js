31339f781a42216a6e076a04af856fb6a75cccd1
Aditya Saligrama <saligrama427@gmail.com>
2020-02-07 23:18:06 -0500

    Update status

--- main.js
+++ main.js
@@ -103,7 +103,7 @@ client.on("ready", () => {
   console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`);
   // Example of changing the bot's playing game to something useful. `client.user` is what the
   // docs refer to as the "ClientUser".
-  client.user.setGame("DM me your confessions");
+  client.user.setActivity("DM confessions here | add !noreact for serious messages");
 
   const table = sql.prepare("SELECT count(*) FROM sqlite_master WHERE type='table' AND name='pool';").get();
   if (!table['count(*)']) {
