9cfcd7c5e77933a9189100b597453fbb6b86575e
Aditya Saligrama <saligrama427@gmail.com>
2020-02-10 09:34:53 -0500

    Fix copyright

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 ﻿/*
- * Copyright (c) 2018 Aditya Saligrama.
+ * Copyright (c) 2020 Aditya Saligrama, Anthony Cui, Emma Hsiao, John Lian.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
