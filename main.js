2d59a54e401290b371e1f4094890c5c2db32e88c
jwlian02 <34924978+jwlian02@users.noreply.github.com>
2020-02-17 23:31:16 -0500

    dadfd

--- main.js
+++ main.js
@@ -226,13 +226,15 @@ client.on("message", async message => {
   client.channels.get(logChannel).send(confessionReturn);
   //message.channel.send(confessionReturn);
   //Make it so that the bot does not respond to confessions so we can delete our degeneracy :)
+  message.react(👍);
+  /*
   message.react(🇸);
   message.react(🇦);
   message.react(🇻);
   message.react(🇪);
   message.react(🇩);
   message.react(👍);
-  /*
+  
   message.channel.send(new Discord.RichEmbed()
       .setColor('#88c0d0')
       .setTitle('Success')
