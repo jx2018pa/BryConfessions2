499e455108ba84962d34c3c99daf27543f212b01
John <jwlian02@gmail.com>
2020-07-21 16:31:01 -0400

    Clean up comments

--- main.js
+++ main.js
@@ -29,21 +29,11 @@ const md5 = require('md5');
 const fs = require('fs');
 let cNum = parseInt(fs.readFileSync("confnum.txt", "utf8"));
 const secret = parseInt(fs.readFileSync("secretkey.txt", "utf8"));
-//console.log(cNum);
-
-// This is your client. Some people call it `bot`, some people call it `self`,
-// some might call it `cootchie`. Either way, when you see `client.something`, or `bot.something`,
-// this is what we're refering to. Your client.
 const client = new Discord.Client();
 
-// Here we load the config.json file that contains our token and our prefix values.
 const auth = require("./auth.json");
 
-// config.token contains the bot's token
-// config.prefix contains the message prefix.
 
-//const SQLite = require("better-sqlite3");
-//const sql = new SQLite("./pool.sqlite");
 
 const banList = require("./banlist.json"); //this should never be uploaded publicly
 const config = require("./config.json");
@@ -71,25 +61,13 @@ let currentPollTitle = "";
 let currentPollOpt1 = "";
 let currentPollOpt2 = "";
 let pollEndTime = 5;
-//let repPostReporters=[[]];
 var args;
 var userInd;
 var options;
 var s,v;
 var anonyPoll = false;
-//var pool = []
 
-/*
-function timeConverter(UNIX_timestamp) {
-  var a = new Date(UNIX_timestamp);
-  time = a.toLocaleString('en-US');
-  return time;
-}
-*/
 function addReaction() {
-    //if (Math.random() > 0.4) {
-    //  return null;
-    //}
 
     var ret = config.reactions[Math.floor(Math.random() * config.reactions.length)];
     while (ret.includes("[NAME]")) {
@@ -126,86 +104,21 @@ function hashId(authId) {
     return haId;
 }
 
-/*
-function createConfession(userMessage) {
-  var embed = new Discord.RichEmbed()
-    .setColor('#88c0d0')
-    .setTitle('Confession #' + userMessage.id)
-    .setDescription(userMessage.message)
-    //.setFooter("posted at " + timeConverter(userMessage.date));
-  );
-  if (userMessage.reaction != null) {
-    embed = embed.addField('Word of rngesus', userMessage.reaction);
-  }
-  
-  return embed;
-}
-
-
-function selectMessage() {
-  var toSend = client.getMessage.get();
-  if (!toSend) {
-    return null;
-  }
-  client.deletePulled.run(toSend.id);
-  var message = createConfession(toSend);
-  return message;
-}
-*/
 client.on("ready", () => {
-    // This event will run if the bot starts, and logs in, successfully.
     console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`);
-
-    //new RegExp((?<=\[)(.*?)(?=\])); //get conf number
-    // Example of changing the bot's playing game to something useful. `client.user` is what the
-    // docs refer to as the "ClientUser".
     client.user.setActivity("Type \"bryhelp\" or \"bryrules\" for more info");
-
-    /*
-      const table = sql.prepare("SELECT count(*) FROM sqlite_master WHERE type='table' AND name='pool';").get();
-      if (!table['count(*)']) {
-        sql.prepare("CREATE TABLE pool (id INTEGER PRIMARY KEY AUTOINCREMENT, message TEXT, date TEXT, reaction TEXT);").run();
-        sql.pragma("synchronous = 1");
-        sql.pragma("journal_mode = wal"); //pin me to the wal
-      }
-    */
-    /*
-      client.getMessage = sql.prepare("SELECT * FROM pool ORDER BY RANDOM() LIMIT 1;");
-      client.deletePulled = sql.prepare("DELETE FROM pool WHERE id=?");
-      client.pushMessage = sql.prepare("INSERT INTO pool (message, date, reaction) VALUES (@message, @date, @reaction);");
-      client.getLast = sql.prepare("SELECT * FROM pool ORDER BY id DESC LIMIT 1;");
-      client.returnId = sql.prepare("SELECT id FROM pool WHERE message=?");
-    */
-    /*
-      var interval = setInterval(function() {
-        for (var i = 0; i < 5; i++) {
-          var toSend = selectMessage();
-          console.log(toSend);
-          if (toSend != null) {
-            slowChannels.forEach(channel => client.channels.get(channel).send(toSend));
-          }
-        }
-      }, 1800000);
-      */
 });
 
 client.on("guildCreate", guild => {
-    // This event triggers when the bot joins a guild.
     console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
 });
 
 client.on("guildDelete", guild => {
-    // this event triggers when the bot is removed from a guild.
     console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
 });
 
 
 client.on("message", async message => {
-    // This event will run on every single message received, from any channel or DM.
-
-    // It's good practice to ignore other bots. This also makes your bot ignore itself
-    // and not get into a spam loop (we call that "botception").
-    //client.channels.get(instantChannel).send("testing 123");
     if (message.author.bot) {
         return;
     }
@@ -373,12 +286,6 @@ client.on("message", async message => {
                     return;
                 }
             }
-            /*
-            if(repPostReporters[reportIndex].indexOf(message.author.id) > -1) {
-            	message.channel.send("You have already reported confession #"+repPostNum[reportIndex]+"!");
-            	return;
-            }
-            */
             if (repPostVol[reportIndex] >= reportsNeeded) {
                 var reportUserIndex = postIds.indexOf(repPostUser[reportIndex]);
                 postWarn[reportUserIndex] = true;
@@ -668,31 +575,6 @@ client.on("message", async message => {
         );
         return;
     }
-    /*
-      var reaction = addReaction();
-      if (message.content.includes("!noreact")) {
-        reaction = null;
-        message.content = message.content.replace("!noreact", "");
-      }
-    */
-    //  client.pushMessage.run({'message': message.content, 'date': timeConverter(message.createdTimestamp), 'reaction': reaction});
-    //  var id = client.returnId.get(message.content).id;
-    //  var confessionReturn = createConfession({'id': id, 'message': message.content, 'date': timeConverter(message.createdTimestamp), 'reaction': reaction})
-    //  client.channels.get(logChannel).send(confessionReturn);
-    //message.channel.send(confessionReturn);
-    //Make it so that the bot does not respond to confessions so we can delete our degeneracy :)
-    //message.react("🇸");
-    //message.react("🇦");
-    //message.react("🇻");
-    //message.react("🇪");
-    //message.react("💾");
-    //message.react("👍");
-    //message.channel.send(new Discord.RichEmbed()
-    //    .setColor('#88c0d0')
-    //    .setTitle('Success')
-    //    .setDescription('Confession saved!')
-    //);
-
 });
 
 client.login(auth.token);
